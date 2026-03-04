5607059e19d9387a879ec931f34c841ec4efa6cd
UNICK J <jeffreyodeloui@gmail.com>
2026-03-04 13:09:42 -0500

    Corrected version

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ function printOdds(count) {
 console.log("EXERCISE 2:\n==========\n");
 function checkAge(userName, age) {
     if (userName === undefined && age === undefined) {console.log("Age and username parameters missing. Nothing to show.")}
-    let aboveSixteen = "Congrats ${userName}, you can drive";
-    let belowSixteen = "Sorry ${userName}, but you need to wait till you're 16";
+    let aboveSixteen = `Congrats ${userName}, you can drive`;
+    let belowSixteen = `Sorry ${userName}, but you need to wait till you're 16`;
     if (age >= 16) {console.log(aboveSixteen);}
     else {console.log(belowSixteen);}
 }
@@ -40,7 +40,7 @@ function TriangleChecker (x, y, z) {
     if (x === undefined || y === undefined || z === undefined) { return console.log("Provide the sides length bro.")}
         
     let sides = [x, y, z].sort((a, b) => a - b);
-    if(sides[2] > sides[0] + sides[1]) { return console.log("It's not a triangle.");}
+    if(sides[2] >= sides[0] + sides[1]) { return console.log("It's not a triangle.");}
     else {
         if(sides[0] === sides[1] && sides[1] === sides[2]){
             return console.log("It's an equilateral triangle.");}
@@ -53,42 +53,38 @@ function TriangleChecker (x, y, z) {
         }
     }
 }
-//15 days used, 15 days remaining
-// Average daily use: 3.333 GB/day
-// You are EXCEEDING your average daily use (3.73 GB/day),
-// continuing this high usage, you'll exceed your data plan by
-// 11.9 GB.
-// To stay below your data plan, use no more than 2.93 GB/day.
+
 // Exercise 5 Section
 console.log("EXERCISE 5:\n==========\n");
 function dataChecker (planLimit, day, usage){
     let aveDaily = usage/day;
+    let recommendDaily = planLimit /30;
     let remDay = 30-day;
-    if (day >= 30){ return console.log(`Your ${planLimit} GB data plan expired ${day - 30} day(s) ago 😔 `); }
+    if (day > 30){ return console.log(`Your ${planLimit} GB data plan expired ${day - 30} day(s) ago 😔 `); }
+    else if(day === 30){return console.log(`Your ${planLimit} GB data plan expired today.`)}
     
     else if (usage >= planLimit){return console.log("You EXCEEDED your plan available data." +
         " You might experience slow internet")}
     
-    else if(usage < planLimit && aveDaily === 3.333){
+    else if(Math.abs(aveDaily - recommendDaily) < 0.01){
         return console.log(`${day} day(s) used, ${remDay} day(s) remaining\n
-               Average daily use: 3.333 GB/day.\n
-               You're doing pretty good. Your average daily usage is right on point.`)}
+Average daily use: ${recommendDaily} GB/day.\n
+You're doing pretty good. Your average daily usage is right on point.`)}
     
-    else if (usage < planLimit && aveDaily < 3.333){
+    else if (aveDaily < recommendDaily){
         return console.log(`${day} day(s) used,${remDay} day(s) remaining\n
 Your average daily usage is ${aveDaily} GB/day 🛜.
-Which is below the average daily usage recommended: 3.333 GB/day.\n
+Which is below the average daily usage recommended: ${recommendDaily} GB/day.\n
 Continuing this usage, and you will end up with 
 ${planLimit - aveDaily * remDay - usage} GB remaining on your plan.\n
-We recommend to use ${3.33 - aveDaily} GB/day more on average to better benefit from your plan.`)
+We recommend to use ${recommendDaily - aveDaily} GB/day more on average to better benefit from your plan.`)
     }
     
     else {
 return console.log(`${day} day(s) used, ${remDay} day(s) remaining\n
 Your average daily usage is ${aveDaily} GB/day.🛜
-You are EXCEEDING the average daily usage recommended: 3.333 GB/day.\n
+You are EXCEEDING the average daily usage recommended: ${recommendDaily} GB/day.\n
 Continuing this high usage, and you'll exceed your data plan by ${aveDaily * remDay + usage - planLimit} GB\n
 To stay below your data plan, we recommend to use no more than ${(planLimit - usage) / remDay} GB/day on average.`)}
 }
-
 
